9fd37ad4c5fd8d5765a54c5351fe7519abdda8de
MichaelFrizzell <MichaelFrizzell@fullsail.edu>
2013-10-13 05:24:01 -0400

    Cigarettes_PerYear

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,4 +1,16 @@
-// JavaScript
+// Frizzell Michael
+// October 12th 2013
+// Expressions_Personal
+
+var userCigPerDay = prompt("How many cigarettes do you smoke a day?");
+console.log(userCigPerDay);
+userPacksPerYear = userCigPerDay * 365/20;
+console.log(userPacksPerYear);
+var userPricePerPack = prompt("How much does one pack of cigarettes cost you?");
+console.log(userPricePerPack);
+var pricePerYear = userPricePerPack * userPacksPerYear;
+var result = pricePerYear;
+alert ("You spend $" + result + " dollars a year on cigarettes, \n and smoke " + userPacksPerYear + " packs per year. \n QUIT NOW!");
 
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "#00FF00";
