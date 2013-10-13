2e89250df2c4acc7d6bd1c1304d22a906b620250
MichaelFrizzell <MichaelFrizzell@fullsail.edu>
2013-10-13 06:32:48 -0400

    Added_console.log
    added console.log to test math.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -3,11 +3,22 @@
 // Expressions_Industry
 
 var userAvgMilesPer =  prompt("How many miles a gallon does your car get");
+console.log(userAvgMilesPer);
+
 var userMilesDriven1 = prompt("How many miles did you drive Week 1?");
+console.log(userMilesDriven1);
+
 var userMilesDriven2 = prompt("How many miles did you drive Week 2?");
+console.log(userMilesDriven2);
+
 var userMilesDriven3 = prompt("How many miles did you drive Week 3?");
+console.log(userMilesDriven3);
+
 var userMilesDriven4 = prompt("How many miles did you drive Week 4?");
+console.log(userMilesDriven4);
+
 var userPricePerGallon = prompt("How much is a gallon of gas locally?");
+console.log(userPricePerGallon);
 
 var totalMilesDriven = userMilesDriven1 + userMilesDriven2 + userMilesDriven3 + userMilesDriven4;
 var gasTotal = (totalMilesDriven / userAvgMilesPer) * userPricePerGallon;
