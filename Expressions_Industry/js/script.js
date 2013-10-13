7c8d1a529228763efc2b83732233b27059366e34
MichaelFrizzell <MichaelFrizzell@fullsail.edu>
2013-10-13 06:30:36 -0400

    named_variables
    Named variable for delivery driver monthly gas total.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,4 +1,18 @@
-// JavaScript
+// Michael Frizzell
+// October 13 2013
+// Expressions_Industry
+
+var userAvgMilesPer =  prompt("How many miles a gallon does your car get");
+var userMilesDriven1 = prompt("How many miles did you drive Week 1?");
+var userMilesDriven2 = prompt("How many miles did you drive Week 2?");
+var userMilesDriven3 = prompt("How many miles did you drive Week 3?");
+var userMilesDriven4 = prompt("How many miles did you drive Week 4?");
+var userPricePerGallon = prompt("How much is a gallon of gas locally?");
+
+var totalMilesDriven = userMilesDriven1 + userMilesDriven2 + userMilesDriven3 + userMilesDriven4;
+var gasTotal = (totalMilesDriven / userAvgMilesPer) * userPricePerGallon;
+var result = "You drove a total of " + totalMilesDriven + " miles, and spent $" +  +" on gas."
+
 
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "#00FF00";
--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -4,17 +4,25 @@
 
 var userCigPerDay = prompt("How many cigarettes do you smoke a day?");
 console.log(userCigPerDay);
+
 userPacksPerYear = userCigPerDay * 365/20;
 console.log(userPacksPerYear);
+
 var userPricePerPack = prompt("How much does one pack of cigarettes cost you?");
 console.log(userPricePerPack);
+
 var userYearsSmoking = prompt("How many years have you smoked?");
 console.log(userYearsSmoking);
+
 var yearsCombinedCigsSmoked = userYearsSmoking * userPacksPerYear;
+
 var pricePerYear = userPricePerPack * userPacksPerYear;
+
 var priceAllTogether = userPricePerPack * userPacksPerYear * userYearsSmoking;
+
 var result = ("You spend $" + pricePerYear + " dollars a year on cigarettes, \n and smoke " + userPacksPerYear + " packs per year. \n " +
     "That is " + yearsCombinedCigsSmoked + " packs smoked, and a total of $" + priceAllTogether + " dollars. \n QUIT NOW!");
+
 alert (result);
 
 //this changes the css of your body tag to be green
