fd9b55dcda18a0ef054c05dfebe2e0fe26092f1f
MichaelFrizzell <MichaelFrizzell@fullsail.edu>
2013-10-13 06:05:49 -0400

    Total_AmountSpentUsed
    Added code for total amount over years.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -8,9 +8,14 @@ userPacksPerYear = userCigPerDay * 365/20;
 console.log(userPacksPerYear);
 var userPricePerPack = prompt("How much does one pack of cigarettes cost you?");
 console.log(userPricePerPack);
+var userYearsSmoking = prompt("How many years have you smoked?");
+console.log(userYearsSmoking);
+var yearsCombinedCigsSmoked = userYearsSmoking * userPacksPerYear;
 var pricePerYear = userPricePerPack * userPacksPerYear;
-var result = pricePerYear;
-alert ("You spend $" + result + " dollars a year on cigarettes, \n and smoke " + userPacksPerYear + " packs per year. \n QUIT NOW!");
+var priceAllTogether = userPricePerPack * userPacksPerYear * userYearsSmoking;
+var result = ("You spend $" + pricePerYear + " dollars a year on cigarettes, \n and smoke " + userPacksPerYear + " packs per year. \n " +
+    "That is " + yearsCombinedCigsSmoked + " packs smoked, and a total of $" + priceAllTogether + " dollars. \n QUIT NOW!");
+alert (result);
 
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "#00FF00";
