9ee62573f0f70b02316cec5e425e38389ecdcc89
MichaelFrizzell <MichaelFrizzell@fullsail.edu>
2013-10-15 21:09:48 -0400

    Screencast_else_if
    .js after video : Screencast: else if.

--- Screencast_Conditionals/js/script.js
+++ Screencast_Conditionals/js/script.js
@@ -1,7 +1,8 @@
-// Conditional Logic
+// Conditional Logic _ Else If
 
 var kidHeight = 50;
 var minHeight = 48;
+var wParentHeight = 45; //the height kid has to be with parent present
 
 
 // if the child is old enough, print to the console "you can ride!"
@@ -9,9 +10,12 @@ var minHeight = 48;
 if(kidHeight > minHeight){
     // code performed if the condition is true
     console.log("You can ride the coaster!");
+}else if(kidHeight > wParentHeight){
+    //you can ride with a parent present
+    console.log("You can ride, but only with a parent present")
 }else{
-    //code only performed if condition is false
-    console.log("Sorry you got some growing to do first");
+    //sorry you have some growing to do
+    console.log("Sorry kid, you have got some growing to do first");
 }
 
 
