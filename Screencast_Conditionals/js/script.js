e2dc8e98adcd790f33d88cf99d64dd99dad0c8c0
MichaelFrizzell <MichaelFrizzell@fullsail.edu>
2013-10-15 20:34:40 -0400

    Screencast_Conditionals_Example
    After video : Screencast: Conditionals Example

--- Screencast_Conditionals/js/script.js
+++ Screencast_Conditionals/js/script.js
@@ -1,4 +1,13 @@
-// JavaScript
+// Conditional Logic
+
+var oldEnough = true;
+
+// if the child is old enough, print to the console "you can ride!"
+if(oldEnough){
+    // code performed if the condition is true
+    console.log("You can ride the coaster!");
+}
+
 
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "#00FF00";
