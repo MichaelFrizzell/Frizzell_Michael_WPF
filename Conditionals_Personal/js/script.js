78f80bd32835b316aa75fef3585506c1eba85f6e
MichaelFrizzell <MichaelFrizzell@fullsail.edu>
2013-10-17 19:34:46 -0400

    comments
    added comments to conditionals personal.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -14,27 +14,28 @@
 //Will I need to go to the store for more food or sodas?
 //TRIP TO STORE?
 
-var amountOfPeople = prompt("How many people are coming to your party?");
+var amountOfPeople = prompt("How many people are coming to your party?");//user sets variable with prompt
 console.log(amountOfPeople);
 
-var numberOfHotDogs = prompt("How many hot dogs do you have?");
+var numberOfHotDogs = prompt("How many hot dogs do you have?");//user sets variable with prompt
 console.log(numberOfHotDogs);
 
-var numberOfBurgers = prompt("How many burgers do you have?");
+var numberOfBurgers = prompt("How many burgers do you have?");//user sets variable with prompt
 console.log(numberOfBurgers);
 
-var totalFood = numberOfBurgers + numberOfHotDogs;
+var totalFood = numberOfBurgers + numberOfHotDogs;//total amount of food burgers + hog dogs
 
-var numberOfSodas = prompt("How many soda's do you have?");
+var numberOfSodas = prompt("How many soda's do you have?");//user sets variable with prompt
 console.log(numberOfSodas);
 
-var foodPerPerson = prompt("How many hamburgers or hot dogs will each person eat?");
+var foodPerPerson = prompt("How many hamburgers or hot dogs will each person eat?");//user sets variable with prompt
 console.log(foodPerPerson);
 
-var sodaPerPerson = prompt("How many soda's will each person drink?")
-
+var sodaPerPerson = prompt("How many soda's will each person drink?")//user sets variable with prompt
+// If # of people at party * food each person will eat is greater than or equal to the total food
+//OR # of people at party * # of soda's everyone will drink is greater than or equal to the total sodas
 if(amountOfPeople * foodPerPerson >= totalFood || amountOfPeople * sodaPerPerson >= numberOfSodas){
-    console.log("You do not have enough burgers, hot dogs and soda's.\nBetter go to the store!");
-}else{
-    console.log("You have enough burgers, no need to go to the store");
+    console.log("You do not have enough burgers, hot dogs and soda's.\nBetter go to the store!");//print this
+}else{//if not they have enough burgers and don't need to go to the store
+    console.log("You have enough burgers and soda's, no need to go to the store");
 }
