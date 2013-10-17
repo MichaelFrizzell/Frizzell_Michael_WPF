dcb39c70785cf7ba1c8971ac09f9752c199bd472
MichaelFrizzell <MichaelFrizzell@fullsail.edu>
2013-10-17 12:01:02 -0400

    Parachuting_Altitude
    I like to skydive so I am taking the altitude and skill level of a
    diver and determining when they should open the chute.  Still hoping it
    works out like I hope.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,4 +1,19 @@
-// JavaScript
+// Michael Frizzell
+// October 16th 2013
+// Conditionals_Personal
+
+//Most students and tandem skydives open their parachutes at 5000'' +
+//'. Licensed and more experienced jumpers will open usually between 2000' and 3500'.' +
+//' Altitude is safety and time, so you start opening higher for time, and as you become more experienced, ' +
+//'many decide to open a little lower.
+//The type of parachute you are flying have a great influence on opening altitude.
+//    High performance parachutes descend at very high speeds, s
+//o higher opening altitudes make more sense as they give the
+//pilot more time to set up for landing and deal with possible malfunctions.
+//
+//2000' is generally accepted as the lowest opening altitude for safety reasons.
+
+
 
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "#00FF00";
