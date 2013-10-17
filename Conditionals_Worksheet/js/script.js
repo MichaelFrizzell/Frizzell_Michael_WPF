519ed6042172e5cc05b6bc310f14858d16c98d70
MichaelFrizzell <MichaelFrizzell@fullsail.edu>
2013-10-16 23:54:28 -0400

    Movie_Price
    Problem: Movie Ticket Price

--- Conditionals_Worksheet/js/script.js
+++ Conditionals_Worksheet/js/script.js
@@ -19,34 +19,72 @@
 //    console.log("You must stop");
 //}
 
-//Check the Login
-//
-//Make sure the user has the correct username and password. If the username doesn’t match then a specific
-//message for that should be printed to the console. If the password doesn’t match a message should appear
-//for that, etc. Only one error message should appear.
-//    Given:
-//Username entered by user
-//Password entered by user
-//Correct username
-//Correct password
-//Result To Print Out:
+
+
+
+//Check the Login?
 //    “Welcome, (place their username here)!”  - if the username and password is correct
 //“User not found. Try again.” -if the username does not match
 //“Password does not match our records.”  -if the username matches but the password does not
+//
+//var userName = prompt("What is your Username?");
+//console.log(userName);
+//
+//var userPassWord = prompt("Whats is you Password?");
+//console.log(userPassWord);
+//
+//var correctUserName = "Michael";
+//var correctPassWord = "joker";
+//
+//if(userName == correctUserName && userPassWord == correctPassWord){
+//    console.log("Welcome, " + userName + "!");//username and password match.
+//}else if(userName != correctUserName && userPassWord == correctPassWord){
+//    console.log("User not found. Try again");//username does not match.
+//}else{
+//    console.log("Password does not match");
+//}
 
-var userName = prompt("What is your Username?");
-console.log(userName);
 
-var userPassWord = prompt("Whats is you Password?");
-console.log(userPassWord);
+//Movie Ticket Price?
+//    “The ticket price is X”
 
-var correctUserName = "Michael";
-var correctPassWord = "joker";
+var userAge = prompt("What is your age?");
+console.log(userAge);
 
-if(userName == correctUserName && userPassWord == correctPassWord){
-    console.log("Welcome, " + userName + "!");//username and password match.
-}else if(userName != correctUserName && userPassWord == correctPassWord){
-    console.log("User not found. Try again");//username does not match.
+var timeOfDay = prompt("What time does your movie start");
+console.log(timeOfDay);
+
+var moviePrice = 12;
+var discountPrice = 7;
+
+if(userAge <= 10 || userAge >= 55){
+    console.log("discountPrice");
+}else if(timeOfDay <= 3 || timeOfDay >= 5){
+    console.log("discountPrice");
 }else{
-    console.log("Password does not match");
+    console.log("moviePrice");
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
