fe6f370e2db8a0a55918e53c3a091a54c0b916ca
MichaelFrizzell <MichaelFrizzell@fullsail.edu>
2013-10-17 18:13:57 -0400

    Comments
    commented out .js file.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -13,17 +13,17 @@ var userSkillLevel = prompt("Are you a tandem diver or skilled diver?");//Asks u
 console.log("userSkillLevel");//Prints out to console users response.
 
 var userChuteType = prompt("Is your parachute a regular or high performance parachute.");//Asks user what type of chute they are using.
-console.log("userChuteType");
+console.log("userChuteType");//Prints out to console users response.
 
-var skilledDiver = "skilled";
-var tandemDiver = "tandem";
-var regularChute = "regular";
-var highPerformanceChute = "high performance";
+var skilledDiver = "skilled";//var set with string
+var tandemDiver = "tandem";//var set with string
+var regularChute = "regular";//var set with string
+var highPerformanceChute = "high performance";//var set with string
 
-if(userSkillLevel === skilledDiver && userChuteType === highPerformanceChute){
-    console.log("You are a " + skilledDiver + " diver using a " + highPerformanceChute + " parachute.\nPull the cord by 2500 feet.");
-}else if(userSkillLevel === skilledDiver && userChuteType != highPerformanceChute){
-    console.log("You are a " + skilledDiver + " diver and using a " + regularChute + " parachute.\nPull the cord at 3500 feet");
-}else{
-    console.log("You are a " + tandemDiver + " diver.\nPull the cord no later than 5000 feet");
+if(userSkillLevel === skilledDiver && userChuteType === highPerformanceChute){//if user is skilled and has a high performance chute pull cord at 2500 feet
+    console.log("You are a " + skilledDiver + " diver using a " + highPerformanceChute + " parachute.\nPull the cord by 2500 feet.");//tell user his skill and chute type and tell him when to pull cord
+}else if(userSkillLevel === skilledDiver && userChuteType != highPerformanceChute){//if not and users skill level is skilled and has a regular chute pull cord at 3500 feet
+    console.log("You are a " + skilledDiver + " diver and using a " + regularChute + " parachute.\nPull the cord at 3500 feet");//tell user his skill and chute type and tell him when to pull cord
+}else{// If neither are true diver is tandem jumper and must pull cord at 5000 feet.
+    console.log("You are a " + tandemDiver + " diver.\nPull the cord no later than 5000 feet");//tell user his skill and chute type and tell him when to pull cord
 }
