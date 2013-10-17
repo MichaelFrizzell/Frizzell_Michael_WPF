4d3ff8d16b1476b72449e22389397d6c79be3bae
MichaelFrizzell <MichaelFrizzell@fullsail.edu>
2013-10-17 18:05:07 -0400

    Name_Problem
    Name added to .js / as well as the scenario and problem to solve.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,4 +1,11 @@
-// JavaScript
+// Michael Frizzell
+// October 16th 2013
+// Conditionals_Personal
 
-//this changes the css of your body tag to be green
-document.querySelector("body").style.backgroundColor= "#00FF00";
+//Most students and tandem skydives open their parachutes at 5000 feet +
+//Licensed and more experienced jumpers with a normal parachute open usually around 3500 feet. +
+//The type of parachute you are flying have a great influence on opening altitude. +
+//Licensed experienced jumpers with a high performance parachute open usually around 2500 feet. +
+
+//Determine at what altitude a jumper has to open there parachute by depending on there skill level and parachute type.
+
