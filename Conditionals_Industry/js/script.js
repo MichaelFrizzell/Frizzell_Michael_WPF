352bb68c1a6444198217ca7da8371e42b186c3af
MichaelFrizzell <MichaelFrizzell@fullsail.edu>
2013-10-17 20:36:26 -0400

    Question_Answer
    Setup problem and set solution

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,4 +1,27 @@
-// JavaScript
+// Michael Frizzell
+// October 16th 2013
+// Conditionals_Industry
 
-//this changes the css of your body tag to be green
-document.querySelector("body").style.backgroundColor= "#00FF00";
+
+//I get off early every friday if i type x amount of words that week
+//given i type x amount of words per minute
+//and worked x amount of hours that week
+// i can leaved work early friday or not
+//LEAVE EARLY FOR WORK?
+
+var wordsRequiredWeek = prompt("How many words are required this week?");
+console.log(wordsRequiredWeek);
+
+var wordsPerMinute = prompt("How many words do you type a minute?");
+console.log(wordsPerMinute);
+
+var hoursWorked = prompt("How many hours have you worked this week?");
+console.log(hoursWorked);
+
+var totalMinutes = hoursWorked * 60;
+console.log(totalMinutes);
+
+var totalWordsTyped = totalMinutes * wordsPerMinute;
+console.log(totalWordsTyped);
+
+(totalWordsTyped >= wordsRequiredWeek) ? console.log("You have typed " + totalWordsTyped + " words.\nYou can leave EARLY!") : console.log("You have only typed " + totalWordsTyped + "\nBetter keep working!")
