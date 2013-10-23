5aacb1d79b4f514a625712977e04b45816d3b455
MichaelFrizzell <MichaelFrizzell@fullsail.edu>
2013-10-23 19:50:42 -0400

    Problem_Stung_written
    wrote out stung problem and commented out previous problem while i work
    this one out

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -8,20 +8,56 @@
 //radius = diameter/2
 //circumference = pi * radius
 
-var diameter = prompt("What is the diameter of the circle?");
-console.log(diameter);
+//var diameter = prompt("What is the diameter of the circle?");
+//console.log(diameter);
+//
+//var radius = diameter / 2;
+//
+//
+//var total = calcCircumference(radius);
+//
+//function calcCircumference(radius){ //parameters, holds information
+//    var circumference = radius * 3.141592;
+//    return circumference;
+//}
+//
+//console.log(total);
 
-var radius = diameter / 2;
 
 
-var total = calcCircumference(radius);
+//Stung!
 
-function calcCircumference(radius){ //parameters, holds information
-    var circumference = radius * 3.141592;
-    return circumference;
-}
+//It takes 8.666666667 bee stings per pound to kill an animal.
+// Calculate how many bee stings are needed to kill an animal in a function
+//    Given:
+//Victim’s weight (in pounds)
+//Parameter(s) for function:
+//Victim’s weight (in pounds)
+//Return:
+//    Number of Bee stings
+//Result to print to the console:
+//    “It takes X bee stings to kill this animal.
 
-console.log(total);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
