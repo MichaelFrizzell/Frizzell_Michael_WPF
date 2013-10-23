79a41a2b1247cb43aa5abf6b080ca02088c7f93b
MichaelFrizzell <MichaelFrizzell@fullsail.edu>
2013-10-23 15:34:07 -0400

    Screencast_Variable_Scope
    .js after Screencast: Variable Scope

--- Functions_Screencast/js/script.js
+++ Functions_Screencast/js/script.js
@@ -1,17 +1,14 @@
 // Functions: ScreenCast Follow Along
 // Function Execution
 
-function outptMsg(){
-    console.log("Hello World");
-}
+var width = 5;
 
 function calcArea(){
     var width = 20;
     var height = 30;
     var area = width * height;
-    console.log(area);
+    //console.log(area);
 }
 
 calcArea();
-calcArea();
-calcArea();
+console.log(width);
