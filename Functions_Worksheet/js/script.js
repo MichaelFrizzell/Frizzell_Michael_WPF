afa60c986e10c54a63b66a0dc836fc296f8f887d
MichaelFrizzell <MichaelFrizzell@fullsail.edu>
2013-10-23 19:47:00 -0400

    finished_circumference
    Finished problem circumference

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -8,6 +8,20 @@
 //radius = diameter/2
 //circumference = pi * radius
 
+var diameter = prompt("What is the diameter of the circle?");
+console.log(diameter);
+
+var radius = diameter / 2;
+
+
+var total = calcCircumference(radius);
+
+function calcCircumference(radius){ //parameters, holds information
+    var circumference = radius * 3.141592;
+    return circumference;
+}
+
+console.log(total);
 
 
 
