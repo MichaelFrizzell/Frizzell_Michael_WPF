d587a040e0cf6394392806da62bd0d78eb0e6e1c
MichaelFrizzell <MichaelFrizzell@fullsail.edu>
2013-10-23 20:24:39 -0400

    comments
    added comments

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -8,20 +8,20 @@
 //radius = diameter/2
 //circumference = pi * radius
 
-var diameter = prompt("What is the diameter of the circle?");
-console.log(diameter);
+var diameter = prompt("What is the diameter of the circle?");//prompts user asks size of circle
+console.log(diameter);//prints to console size of circle
 
-var radius = diameter / 2;
+var radius = diameter / 2;//sets variable for radius by dividing the diameter by 2
 
-var circleTotal = calcCircumference(radius);
+var circleTotal = calcCircumference(radius);//sets variable for the circumference which is performed in the function below
 
-function calcCircumference(radius){
-    var circumference = radius * 3.141592;
-    return circumference;
+function calcCircumference(radius){//begin function
+    var circumference = radius * 3.141592;//radius * pi for circumference
+    return circumference;//returns the value of circumference to variable circleTotal
 }
 
-var result = ("The circumference of the circle is " + circleTotal);
-console.log(result);
+var result = ("The circumference of the circle is " + circleTotal);//sets results
+console.log(result);//prints results to console
 
 
 
@@ -30,8 +30,8 @@ console.log(result);
 //Calculate how many bee stings are needed to kill an animal in a function
 //“It takes X bee stings to kill this animal."
 
-var animalWeight = prompt("How much does the animal weigh?");
-console.log(animalWeight);
+var animalWeight = prompt("How much does the animal weigh?");//Asks user for animal weight
+console.log(animalWeight);prints to console animal weight
 
 var stingsPerPound = 8.666666667;
 
