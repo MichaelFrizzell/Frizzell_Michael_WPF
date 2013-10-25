0d6f856352c193219ca81d170bd7a0cd60d80f2d
MichaelFrizzell <MichaelFrizzell@fullsail.edu>
2013-10-25 13:08:35 -0400

    problem_created
    setup problem in comments

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,4 +1,34 @@
-// JavaScript
+//Michael Frizzell
+//October 23, 2013
+//Functions_Industry
 
-//this changes the css of your body tag to be green
-document.querySelector("body").style.backgroundColor= "#00FF00";
+//A ternary / if else shorthand / () ? true : if falseXXXX
+
+//An else ifXXXX
+
+//An anonymous / function  var a = function(){console}
+//A normal "named" functionXXXX
+
+//An expression with two arithmetic operators / >= / <= etc...
+
+//A function with three parameters / function funcName(parameter){console} push with argumentXXXX
+//At least one logical operator / && / \\ / !!XXXX
+
+//a diner has three new specials on the menu tonite
+//they want the servers to push the three specials
+//the management tells the servers if they can sell 20 or more of the three specials that night they will get 5% of all the specials they sell that nite
+//If the server sells more than 40 they get an extra 20 dollars on top of the 5% bonus
+// set up a function to determine if the server sold enough specials
+//set up a conditional that tells the computer what to do if they did or did not sell enough of the specials
+//tell the server how much there bonus is if they sold enough based on the specials being x amount of dollars
+
+
+
+
+
+
+
+
+
+
+
