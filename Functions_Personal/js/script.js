87947facf3a93788d11559759c75b59190106c1b
MichaelFrizzell <MichaelFrizzell@fullsail.edu>
2013-10-24 20:28:31 -0400

    problem_established
    established problem and started solution

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,4 +1,33 @@
-// JavaScript
+//Michael Frizzell
+//October 23, 2013
+//Functions_Personal
 
-//this changes the css of your body tag to be green
-document.querySelector("body").style.backgroundColor= "#00FF00";
+//A ternary / if else shorthand / () ? true : if falseXXXX
+//An else if
+//An anonymous / function  var a = function(){console}
+//A normal "named" function
+//An expression with two arithmetic operators / >= / <= etc...
+//A function with three parameters / function funcName(parameter){console} push with argument
+//At least one logical operator / && / \\ / !!
+
+//It is Halloween Night
+//You have X peices of candy
+//kids under 3feet tall get 1 peice
+//kids over 3 feet get 2 peices
+//if X amount of under3 feet kids come
+//and X amount of over3 feet kids come
+//Do you have to get more candy
+
+var userCandy = prompt("How many peices of candy do you have?");
+console.log(userCandy);
+
+var kidsUnder3 = prompt("How many kids under 3 feet tall");
+console.log(kidsUnder3);
+
+var kidsOver3 = prompt("How many kids over 3 feet tall");
+console.log(kidsOver3);
+
+var totalNeeded = kidsUnder3 + kidsOver3 * 2;
+
+result = (userCandy >= totalNeeded) ? "You have enough Candy" : "Better go get more candy" ;
+console.log(result);
