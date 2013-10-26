40ffa9243b26e9edf0f4f05a40a5215caf40b181
MichaelFrizzell <MichaelFrizzell@fullsail.edu>
2013-10-25 20:17:51 -0400

    commented_out
    cleaned up code and commented

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -8,27 +8,27 @@
 //if over x amount of miles per hour tell user to slow down
 //else drive faster
 
-var milesDriven = prompt("How many miles did you drive?");
-console.log(milesDriven);
+var milesDriven = prompt("How many miles did you drive?");//gets user information
+console.log(milesDriven);//prints user info to console
 
-var timeDriven = prompt("How many minutes did you drive?");
-console.log(timeDriven);
+var timeDriven = prompt("How many minutes did you drive?");//gets user information
+console.log(timeDriven);//prints user info to console
 
-var milesPerHour = function(milesDriven, timeDriven){
-    var perHour = Number(milesDriven) / Number(timeDriven);
-    return perHour;
+var milesPerHour = function(milesDriven, timeDriven){//anonymous function that divides the milesDriven by timeDriven
+    var perHour = Number(milesDriven) / Number(timeDriven);//function completed
+    return perHour;//returns perHour variable to variable result
 }
 
-var result = milesPerHour(milesDriven, timeDriven);
-console.log(result);
+var result = milesPerHour(milesDriven, timeDriven);//puts function into variable
+console.log(result);//prints result to console
 
-var maxPerHour = 60;
-console.log(maxPerHour);
+var maxPerHour = 60;//sets variable for maximum hours per hour
+console.log(maxPerHour);//prints maxPerHour to console
 
-if(milesPerHour >= maxPerHour){
-    console.log("You have driven " + result + " miles.\nSLOW DOWN!");
+if(milesPerHour >= maxPerHour){//ifelse conditional statement,
+    console.log("You have driven " + result + " miles.\nSLOW DOWN!");//if milesPerHour are greater or equal to maxPerHour
 }else{
-    console.log("You have only driven " + result + " miles.\nSPEED UP!");
+    console.log("You have only driven " + result + " miles.\nSPEED UP!");//else print this to console
 }
 
 
